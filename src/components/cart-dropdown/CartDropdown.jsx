4ac3a4a844741ba4478899ce3e6ffac26c008927
const-kalty <anjolaoluwa055@gmail.com>
2023-01-27 15:20:00 +0100

    addToCart features newly added

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Button from '../button/Button'
-import './cartDropdown.styles.scss'
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import Button from "../button/Button";
+import CartItem from "../cart-item/CartItem";
+import "./cartDropdown.styles.scss";
 
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
   return (
-    <div className='cart-dropdown-container'>
-<div className="card-items"/> 
-<Button>GO TO CHECKOUT</Button>
+    <div className="cart-dropdown-container">
+      <div className="card-items">
+       {cartItems.length ? (cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)) :(
+        <span className="empty-message">Your cart is empty</span>
+       )}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
     </div>
-  )
-}
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
